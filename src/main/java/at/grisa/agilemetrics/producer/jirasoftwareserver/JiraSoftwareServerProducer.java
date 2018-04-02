3eefbfbd604e7315d191922dc2e98e513f696196
daniel <dgr7348@students.fhv.at>
2018-04-02 10:07:21 +0200

    API readme

---
+++ src/main/java/at/grisa/agilemetrics/producer/jirasoftwareserver/JiraSoftwareServerProducer.java
@@ -0,0 +1,11 @@
+package at.grisa.agilemetrics.producer.jirasoftwareserver;
+
+import at.grisa.agilemetrics.cron.MeasurementQueue;
+import at.grisa.agilemetrics.producer.IProducer;
+
+public class JiraSoftwareServerProducer implements IProducer {
+    @Override
+    public void produce(MeasurementQueue measurementQueue) {
+
+    }
+}
