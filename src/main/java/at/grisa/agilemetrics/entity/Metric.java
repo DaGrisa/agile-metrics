2a5279fe4d8ca514d1f901850592876290f27742
daniel <dgr7348@students.fhv.at>
2018-04-26 10:40:57 +0200

    add sonarcloud

--- src/main/java/at/grisa/agilemetrics/entity/Metric.java
+++ src/main/java/at/grisa/agilemetrics/entity/Metric.java
@@ -1,26 +1,29 @@
 package at.grisa.agilemetrics.entity;
 
 import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public final class Metric {
     public final Double value;
     public final String name;
     public final Map meta;
     public final LocalDate date;
+    public final Set<String> tags;
 
-    public Metric(Double value, String name, HashMap<String, String> meta) {
+    public Metric(Double value, String name, Map<String, String> meta) {
         this.value = value;
         this.name = name;
         this.meta = meta;
         this.date = LocalDate.now();
+        tags = null;
     }
 
-    public Metric(Double value, String name, HashMap<String, String> meta, LocalDate date) {
+    public Metric(Double value, String name, Map<String, String> meta, Set<String> tags) {
         this.value = value;
         this.name = name;
         this.meta = meta;
-        this.date = date;
+        this.tags = tags;
+        this.date = LocalDate.now();
     }
 }
